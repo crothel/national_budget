9f576c89f0336e089de2f020ddf83a3a849b8695
crothel <crothel.7m4ovy@gmail.com>
2018-09-02 14:48:18 +0900

    パス修正

--- convert.py
+++ convert.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 
-file = pd.read_excel('/Users/t-matsuzawa/budget_viz/national_budget/data/DL201811001.xls', sheet_name = '一般会計 予定経費要求書（科目別内訳）')
+file = pd.read_excel('./data/DL201811001.xls', sheet_name = '一般会計 予定経費要求書（科目別内訳）')
 file = file.iloc[:,[0,1,9,10,11]]
 file.columns = ["jurisdiction", "organization", "item", "subject", "amount"]
 file[["jurisdiction", "organization", "item"]] = file[["jurisdiction", "organization", "item"]].fillna(method="ffill")
